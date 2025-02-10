a249cdc673a5bc89f6b3f65594b4798ec2794a56
WabukoWabuko <basilwabbs@gmail.com>
2025-02-10 21:18:21 +0000

    Slight changes here and there, wanna rest my head

--- Project/community/views.py
+++ Project/community/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from .models import Rating
+from .models import Rating, CommunityPost
 from .serializers import RatingSerializer
 from .permissions import IsAuthorOrReadOnly
 
@@ -11,3 +11,10 @@ class RatingViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class CommunityPostViewSet(viewsets.ModelViewSet):
+    queryset = CommunityPost.objects.all()
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
